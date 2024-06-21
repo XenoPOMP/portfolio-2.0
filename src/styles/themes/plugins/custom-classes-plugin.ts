475d9e22924722c8640c9431571335ee463e196d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-21 14:32:58 +0300

    Revert "✍️ create tw utils"
    This reverts commit 95d3cccf81a6990f343ded186de5dff2ff2c5056.

--- src/styles/themes/plugins/custom-classes-plugin.ts
+++ src/styles/themes/plugins/custom-classes-plugin.ts
@@ -1,31 +1,19 @@
 import plugin from 'tailwindcss/plugin';
 
 import { cssPropertiesToTw } from './api';
-import { createFonts } from './create-fonts.ts';
 
 /**
  * Add custom classes and utilities to tailwind.
  * @constructor
  */
 export const CustomClassesPlugin = () => {
-  return plugin(({ addComponents, matchUtilities }) => {
+  return plugin(({ addComponents }) => {
     addComponents({
       '.flex-center': cssPropertiesToTw({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
       }),
-    });
-
-    matchUtilities({
-      // Create dynamic utils for --p{number}
-      ...createFonts(
-        size => ({
-          tailwind: `p${size}`,
-          css: `--p${size}`,
-        }),
-        ...[24, 20, 16, 14],
-      ),
     });
   });
 };
