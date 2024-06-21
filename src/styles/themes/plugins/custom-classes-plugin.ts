46ef7e931fc31a678d95802186b0ca3b39c1a9ac
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-21 14:49:41 +0300

    🔗 create var utils

--- src/styles/themes/plugins/custom-classes-plugin.ts
+++ src/styles/themes/plugins/custom-classes-plugin.ts
@@ -2,18 +2,47 @@ import plugin from 'tailwindcss/plugin';
 
 import { cssPropertiesToTw } from './api';
 
+const createFont = (size: number) => ({
+  [`p${size}`]: (mod = '1') =>
+    cssPropertiesToTw({
+      fontSize: `calc(var(--p${size}) * ${mod})`,
+      lineHeight: '140%',
+    }),
+});
+
+const createHeading = (lvl: number) => ({
+  [`h${lvl}`]: (mod = '1') =>
+    cssPropertiesToTw({
+      fontSize: `calc(var(--heading-${lvl}) * ${mod})`,
+      lineHeight: 'normal',
+    }),
+});
+
 /**
  * Add custom classes and utilities to tailwind.
  * @constructor
  */
 export const CustomClassesPlugin = () => {
-  return plugin(({ addComponents }) => {
+  return plugin(({ addComponents, matchUtilities }) => {
     addComponents({
       '.flex-center': cssPropertiesToTw({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
       }),
+    });
+
+    matchUtilities({
+      ...createFont(24),
+      ...createFont(20),
+      ...createFont(16),
+      ...createFont(14),
+
+      ...createHeading(1),
+      ...createHeading(2),
+      ...createHeading(3),
+      ...createHeading(4),
+      ...createHeading(5),
     });
   });
 };
