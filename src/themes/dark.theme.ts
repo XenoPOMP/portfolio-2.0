225e451cddd84bbc4a539b2ce6b6274c28294768
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-21 14:06:46 +0300

    🎨 update themes

--- src/themes/dark.theme.ts
+++ src/themes/dark.theme.ts
@@ -2,9 +2,24 @@ import { type CustomTheme } from './index';
 
 export const darkTheme: CustomTheme = {
   colors: {
-    primary: {
-      bg: '#0d1117',
-      font: '#FFF',
-    },
+    highlight: '#22C55E',
+    'highlight-font': '#000000',
+    primary: '#0F0E0E',
+    'nav-highlight': '#2678C2',
+    'social-media-bg': '#252424',
+    'social-media-icon': '#9A9A9A',
+    'social-media-icon-inactive': '#575252',
+    'font-primary': '#FFFFFF',
+    'font-secondary': '#B9B5B5',
+    'font-third-rate': '#89C4C8',
+    'project-preview-primary': '#432237',
+    'btn-primary': '#FFFFFF',
+    'btn-secondary': '#000000',
+    'footer-primary': '#000000',
+    'footer-secondary': '#FFFFFF',
+    'tooltip-bg': '#FFFFFF',
+    'tooltip-font': '#000000',
+    'og-tag-next-primary': '#000000',
+    'og-tag-next-secondary': '#FFFFFF',
   },
 };
