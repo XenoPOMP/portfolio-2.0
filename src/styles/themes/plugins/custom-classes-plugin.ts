95d3cccf81a6990f343ded186de5dff2ff2c5056
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-21 14:28:25 +0300

    ✍️ create tw utils

--- src/styles/themes/plugins/custom-classes-plugin.ts
+++ src/styles/themes/plugins/custom-classes-plugin.ts
@@ -1,19 +1,31 @@
 import plugin from 'tailwindcss/plugin';
 
 import { cssPropertiesToTw } from './api';
+import { createFonts } from './create-fonts.ts';
 
 /**
  * Add custom classes and utilities to tailwind.
  * @constructor
  */
 export const CustomClassesPlugin = () => {
-  return plugin(({ addComponents }) => {
+  return plugin(({ addComponents, matchUtilities }) => {
     addComponents({
       '.flex-center': cssPropertiesToTw({
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
       }),
+    });
+
+    matchUtilities({
+      // Create dynamic utils for --p{number}
+      ...createFonts(
+        size => ({
+          tailwind: `p${size}`,
+          css: `--p${size}`,
+        }),
+        ...[24, 20, 16, 14],
+      ),
     });
   });
 };
