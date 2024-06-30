61064d7c7a3a63a9f166810a6529415faea5e4cd
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-30 19:05:01 +0300

    📝 update project

--- src/data/projects/entries/smart-ace.project.ts
+++ src/data/projects/entries/smart-ace.project.ts
@@ -1,20 +1,21 @@
-import preview from '@/public/mocks/mock-preview.png';
 import { Priority, ProjectFactory } from '@/src/data/projects';
 
 export const SmartAceProject = new ProjectFactory()
   .name('Вебсайт SmartAce')
-  .desc('Complex app')
-  .previewImg(preview)
+  .desc(
+    'Этот проект - это мой курсовой проект за 2023 год. Тема проекта - веб-приложение для компании, предоставляющей IT-услуги.',
+  )
   .priority(Priority.medium)
   .frontendStack({
     react: true,
+    vite: true,
     typescript: true,
-    nextjs: true,
+    redux: true,
   })
   .backendStack({
     nest: true,
-    prisma: true,
+    mssql: true,
   })
-  .website('https://www.xenoplanner-dev.ru')
-  .oss('https://github.com/XenoPOMP/xeno-planner')
+  .website('https://xeno-coursework-2023.netlify.app/')
+  .oss('https://github.com/XenoPOMP/coursework-2023')
   .buildup();
