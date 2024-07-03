bf76e876d2201dfb9d44711d012efc9aaa0ec5da
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-07-03 20:23:04 +0300

    ✍️ update smart-ace.project

--- src/data/projects/entries/smart-ace.project.ts
+++ src/data/projects/entries/smart-ace.project.ts
@@ -1,3 +1,4 @@
+import preview from '@/public/previews/smart-ace-preview.png';
 import { Priority, ProjectFactory } from '@/src/data/projects';
 
 export const SmartAceProject = new ProjectFactory()
@@ -5,6 +6,7 @@ export const SmartAceProject = new ProjectFactory()
   .desc(
     'Этот проект - это мой курсовой проект за 2023 год. Тема проекта - веб-приложение для компании, предоставляющей IT-услуги.',
   )
+  .previewImg(preview)
   .priority(Priority.medium)
   .frontendStack({
     react: true,
